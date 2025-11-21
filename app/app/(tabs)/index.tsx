37199988bc3918028ba1536593270ef1009bcba3
Emberwind-Studios <89319126+Emberwind-Studios@users.noreply.github.com>
2025-11-21 08:17:17 -0500

    Updated design and added new rescan button

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -1,55 +1,112 @@
-import { Platform, StyleSheet, TouchableOpacity, View, Text } from 'react-native';
-import React, { useEffect, useState } from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import BleManager from 'react-native-ble-manager';
+import {
+  Platform,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  ScrollView,
+  Button,
+  StatusBar,
+  PermissionsAndroid,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import BleManager, { Peripheral } from "react-native-ble-manager";
+import { getCustomTabsSupportingBrowsersAsync } from "expo-web-browser";
 
 export default function HomeScreen() {
-
   const [bleInit, setBleInit] = useState<boolean>(false);
+  const [deviceInfo, setDeviceInfo] = useState<Peripheral[]>([]);
+  const [restartScan, setRestartScan] = useState<boolean>(false);
 
   //initialize bluetooth manager
   useEffect(() => {
-    BleManager.start({ showAlert: true });
+    BleManager.start({ showAlert: true }).then(() => {
+      console.log("BLE initialized");
+
+      //Start Scan
+      const scanOptions = { seconds: 5, allowDuplicates: false };
+      BleManager.scan(scanOptions).then(() => {
+        // Success code
+        console.log(scanOptions);
+        console.log("Scan started");
+      });
+    });
 
     BleManager.isStarted().then((started) => {
       setBleInit(true);
-      console.log(`Module is ${started ? '' : 'not '}started`);
+      console.log(`Module is ${started ? "" : "not "}started`);
     });
 
-    const onStopListener = BleManager.onStopScan((args) => {
+    const onStopListener = BleManager.onStopScan(() => {
       console.log("scan finished");
-      BleManager.getConnectedPeripherals([]).then((peripheralsArray) => {
-        // Success code
-        console.log("Connected peripherals: " + peripheralsArray.length);
-      });
-    })
+      setRestartScan(false);
+    });
+
+    const onDiscoveredPeripheralListener = BleManager.onDiscoverPeripheral(
+      (peripheral: Peripheral) => {
+        const { id, name } = peripheral;
+
+        console.log("found device");
+        console.log(`Device Id: ${id}`);
+        console.log(`Device Name: ${name ?? "Unknown"}`);
+
+        setDeviceInfo((prev) => {
+          // const exists = prev.some((p) => p.id === peripheral.id);
+
+          return [...prev, peripheral];
+        });
+      }
+    );
 
     return () => {
       onStopListener.remove();
+      onDiscoveredPeripheralListener.remove();
     };
-  }, [])
-
-
-  if (bleInit) {
-    const scanOptions = {seconds:5};
-    BleManager.scan(scanOptions).then(() => {
-      // Success code
-      console.log(scanOptions);
-      console.log("Scan started");
-    });
-  }
+  }, [restartScan]);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.heartRateText}>Content is in safe area.</Text>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+      <StatusBar
+        translucent={false} // ensures content starts below the status bar
+        barStyle="dark-content"
+        backgroundColor="white" // match SafeAreaView
+      />
+      <View style={{ flex: 1, paddingHorizontal: 20 }}>
+        <Text style={styles.heartRateText}>Devices Bitch.</Text>
+        <ScrollView style={{ flex: 1, marginTop: 15 }}>
+          {deviceInfo.length > 0 &&
+            deviceInfo.map((device) => (
+              <View key={device.id} style={{ marginBottom: 12 }}>
+                <Text style={{ color: "black" }}>{device.id}</Text>
+                <Text style={{ color: "black" }}>
+                  {device.name ?? "Unknown"}
+                </Text>
+              </View>
+            ))}
+        </ScrollView>
+        <View style={styles.ctaButton}>
+          <Button
+            title="Rescan Devices"
+            onPress={() => {
+              setDeviceInfo([]);
+              setRestartScan(true);
+            }}
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f2f2f2",
+    justifyContent: "center",
+    backgroundColor: "#6495ED",
+    paddingTop: StatusBar.currentHeight,
+    borderTopWidth: 2,
+    borderTopColor: "black",
   },
   heartRateTitleWrapper: {
     flex: 1,
@@ -68,13 +125,14 @@ const styles = StyleSheet.create({
     marginTop: 15,
   },
   ctaButton: {
-    backgroundColor: "#FF6060",
+    backgroundColor: "#6495ED",
     justifyContent: "center",
     alignItems: "center",
     height: 50,
     marginHorizontal: 20,
     marginBottom: 5,
     borderRadius: 8,
+    marginTop: 20,
   },
   ctaButtonText: {
     fontSize: 18,
@@ -82,4 +140,3 @@ const styles = StyleSheet.create({
     color: "white",
   },
 });
-
