3f92730cee1691ec5a9d506b1394db0f2809248f
Kaleb Pierce <kalebpierce@gmail.com>
2026-04-02 17:00:16 -0400

    removed uneeded packages

--- app/src/app/(drawer)/about/userManual/user-manual.tsx
+++ app/src/app/(drawer)/about/userManual/user-manual.tsx
@@ -1,31 +1,31 @@
-import { StyleSheet, View, Dimensions } from "react-native";
-import Pdf from "react-native-pdf";
+// import { StyleSheet, View, Dimensions } from "react-native";
+// import Pdf from "react-native-pdf";
 
-export default function UserManual() {
-  const source = {
-    uri: "https://www.hydro-bioscience.com/wp-content/uploads/2023/06/My-Quattro-App-User-Manual.pdf",
-    cache: true,
-  };
+// export default function UserManual() {
+//   const source = {
+//     uri: "https://www.hydro-bioscience.com/wp-content/uploads/2023/06/My-Quattro-App-User-Manual.pdf",
+//     cache: true,
+//   };
 
-  return (
-    <View style={styles.container}>
-      <Pdf
-        source={source}
-        style={styles.pdf}
-        onError={(error) => {
-        }}
-      />
-    </View>
-  );
-}
+//   return (
+//     <View style={styles.container}>
+//       <Pdf
+//         source={source}
+//         style={styles.pdf}
+//         onError={(error) => {
+//         }}
+//       />
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  pdf: {
-    flex: 1,
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+//   pdf: {
+//     flex: 1,
+//     width: Dimensions.get("window").width,
+//     height: Dimensions.get("window").height,
+//   },
+// });
